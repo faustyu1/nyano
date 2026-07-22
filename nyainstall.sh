65acd19d1948392b3b48fe6575f8002b270c69fe
shareui <shareui@proton.me>
2026-07-22 16:12:00 +0300

    fix deleting

--- nyainstall.sh
+++ nyainstall.sh
@@ -4,7 +4,7 @@ if ! command -v cargo >/dev/null 2>&1; then
     echo "Error: Cargo not found"
     echo "Use this command to install the Rust setup:"
     echo "    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh && source \"\$HOME/.cargo/env\""
-    rm -- "$0"
+    rm nyainstall.sh
     exit 1
 fi
 
@@ -16,4 +16,5 @@ echo "Installing to /usr/bin/nyano"
 sudo install -m 755 target/release/nyano /usr/bin/nyano
 
 printf '\033[32mSuccessfully installed to %s!\033[0m\n' "/usr/bin/nyano"
-rm -- "$0"
+rm nyainstall.sh
+rm nyano
